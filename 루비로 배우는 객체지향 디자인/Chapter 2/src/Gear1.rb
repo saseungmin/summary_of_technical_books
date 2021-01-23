5030f0fc35d0616f43503fb25626f1064b021aac
saseungmin <dbd02169@naver.com>
2021-01-23 19:24:10 +0900

    [Update] Object-Oriented-in-ruby-chapter-2

---
+++ 루비로 배우는 객체지향 디자인/Chapter 2/src/Gear1.rb
@@ -0,0 +1,14 @@
+class Gear
+  attr_reader :chainring, :cog
+  def initialize(chainring, cog)
+    @chainring = chainring
+    @cog = cog
+  end
+
+  def ratio
+    chainring / cog.to_f
+  end
+end
+
+puts Gear.new(52, 11).ratio
+puts Gear.new(30, 27).ratio
