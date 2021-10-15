9387d54c0067e39dbaa202d9ea011c701625ed4c
saseungmin <dbd02169@naver.com>
2021-10-15 21:38:23 +0900

    [Refactor] Summarize books 사이트에 관한 Refactoring을 해라
    - Refactor Summarize books Site

--- src/components/RowSummaryBooksWrapper.jsx
+++ src/components/RowSummaryBooksWrapper.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 
-import { v4 as uuidv4 } from 'uuid';
-
 import styled from '@emotion/styled';
+
+import { v4 as uuidv4 } from 'uuid';
 
 import SummaryBookTemplate from './SummaryBookTemplate';
 
